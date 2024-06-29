d4d3e0196cc360dcf8b243e1b5cabecf0454b4b3
Nayisw <mrprogaming003@gmail.com>
2024-06-29 15:10:58 +0530

    commit

---
+++ video54/script.js
@@ -0,0 +1,22 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to feed Froggie? (he's hungry) ")
+
+if(isTrue){
+    console.log("Froggie is hungry")
+}
+
+else{
+    console.log("Gugugaga!")
+
+}
+console.log("Your number is " + a)
+
+document.title = "Hey I am Froggie"
+
+// document.body.style.backgroundColor = "red"
